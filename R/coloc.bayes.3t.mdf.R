e13ace301e199903eeeebdb31fabc49aa04b6262
mdfortune <mdf34@cam.ac.uk>
2013-11-01 11:20:20 +0000

    Update coloc.bayes.3t.mdf.R

--- R/coloc.bayes.3t.mdf.R
+++ R/coloc.bayes.3t.mdf.R
@@ -10,7 +10,7 @@
 #'@param response The name of the response variable in \code{df1}
 #'@param priors A list of priors over the hypotheses 
 #'@param pp.thr posterior probability threshold used to trim SNP list.  Only SNPs with a marginal posterior probability of inclusion greater than this with one or other trait will be included in the full BMA analysis
-#'@param r2.trim for pairs SNPs with r2>\code{r2.trim}, only one SNP will be retained.  This avoids numerical instability problems caused by including two highly correlated SNPs in the model.
+#'@param r2.trim for pairs SNPs with r2 greater than \code{r2.trim}, only one SNP will be retained.  This avoids numerical instability problems caused by including two highly correlated SNPs in the model.
 #'@param quiet suppress messages about how the model spaced is trimmed for BMA
 #'@return a list of posterior probabilities that each SNP is causitive to both traits, and the corresponding SNPs
 #'@author Mary Fortune
@@ -40,24 +40,100 @@ coloc.bayes.3t <- function(df1,snps=setdiff(colnames(df1),response),response="Y"
     }
     n.clean <- length(snps)
     #remove SNPs with low posterior probabilities in the individual models.
-    f1 <- as.formula(paste("Y ~", paste(snps,collapse="+")))
-    df.trait1<-df1[which(df1[,1] %in% c(0,1)),c("Y",snps)]
-    df.trait2<-df1[which(df1[,1] %in% c(0,2)),c("Y",snps)]
-    df.trait3<-df1[which(df1[,1] %in% c(0,3)),c("Y",snps)]
-    df.trait2[,1]<-df.trait2[,1]/2
-    df.trait3[,1]<-df.trait3[,1]/3
-    modelsep<-diag(n.clean)
-    mod.trait1<-glib(x=df.trait1[,-1], y=df.trait1[,1], error="binomial", link="logit",models=modelsep)
-    mod.trait2<-glib(x=df.trait2[,-1], y=df.trait2[,1], error="binomial", link="logit",models=modelsep)
-    mod.trait3<-glib(x=df.trait3[,-1], y=df.trait3[,1], error="binomial", link="logit",models=modelsep)
-    pp.trait1<-mod.trait1$bf$postprob[,2]
-    pp.trait2<-mod.trait2$bf$postprob[,2]
-    pp.trait3<-mod.trait3$bf$postprob[,2]
-    whichsnps<-sort(union(union(which(pp.trait1>pp.thr),which(pp.trait2>pp.thr)),which(pp.trait3>pp.thr)))
-    cat("We consider ",length(whichsnps), " SNPs in the final analysis. \n")
-    snps<-snps[whichsnps]
-    
-    n.clean <- length(snps)
+	#extract just those samples relating to trait1
+	df.trait1<-df1[which(df1[,1]%in% c(0,1)),c("Y",snps)]
+	#remove columns if necessary to ensure a full rank X
+	intX1<-cbind(Intercept=1,df.trait1[,-1])
+	intrank<-qr(intX1)$rank
+	if ( intrank < ncol(intX1)){
+		#we must remove at k columns
+		k<-ncol(intX1)-intrank
+		drop<-c()
+		for(ii in 1:n.clean) { 
+			if (length(drop)<k){ 
+				xsub <- intX1[,setdiff(colnames(intX1),snps[ii])]
+				droprank<- qr(xsub)$rank
+				if (droprank==intrank){
+					drop<-c(drop,snps[ii])
+					intX1<-xsub
+				}
+			}
+		}
+		snps<-setdiff(snps,drop)
+		df.trait1<-df.trait1[,- which(colnames(df.trait1) %in% drop)]
+		df1<-df1[,-which(colnames(df1) %in% drop)]
+		n.clean <- length(snps)
+	}
+	#run the model
+	f1 <- as.formula(paste("Y ~", paste(snps,collapse="+")))
+	modelsep<-diag(n.clean)
+	mod.trait1<-glib(x=df.trait1[,-1], y=df.trait1[,1], error="binomial", link="logit",models=modelsep)
+	pp.trait1<-mod.trait1$bf$postprob[,2]
+	whichsnps1<-snps[which(pp.trait1>pp.thr)]
+	#extract just those samples relating to trait2
+	df.trait2<-df1[which(df1[,1]%in% c(0,2)),c("Y",snps)]
+	#remove columns if necessary to ensure a full rank X
+	intX2<-cbind(Intercept=1,df.trait2[,-1])
+	intrank<-qr(intX2)$rank
+	if ( intrank < ncol(intX2)){
+		#we must remove at k columns
+		k<-ncol(intX2)-intrank
+		drop<-c()
+		for(ii in 1:n.clean) { 
+			if (length(drop)<k){ 
+				xsub <- intX2[,setdiff(colnames(intX2),snps[ii])]
+				droprank<- qr(xsub)$rank
+				if (droprank==intrank){
+					drop<-c(drop,snps[ii])
+					intX2<-xsub
+				}
+			}
+		}
+		snps<-setdiff(snps,drop)
+		df.trait2<-df.trait2[,- which(colnames(df.trait2) %in% drop)]
+		df2<-df2[,-which(colnames(df2) %in% drop)]
+		n.clean <- length(snps)
+	}
+	#run the model
+	f2 <- as.formula(paste("Y ~", paste(snps,collapse="+")))
+	modelsep<-diag(n.clean)
+	mod.trait2<-glib(x=df.trait2[,-1], y=df.trait2[,1]/2, error="binomial", link="logit",models=modelsep)
+	pp.trait2<-mod.trait2$bf$postprob[,2]
+	whichsnps2<-snps[which(pp.trait2>pp.thr)]
+	#extract just those samples relating to trait3
+	df.trait3<-df1[which(df1[,1] %in% c(0,3)),c("Y",snps)]
+	#remove columns if necessary to ensure a full rank X
+	intX3<-cbind(Intercept=1,df.trait3[,-1])
+	intrank<-qr(intX3)$rank
+	if ( intrank < ncol(intX3)){
+		#we must remove at k columns
+		k<-ncol(intX3)-intrank
+		drop<-c()
+		for(ii in 1:n.clean) { 
+			if (length(drop)<k){ 
+				xsub <- intX3[,setdiff(colnames(intX3),snps[ii])]
+				droprank<- qr(xsub)$rank
+				if (droprank==intrank){
+					drop<-c(drop,snps[ii])
+					intX3<-xsub
+				}
+			}
+		}
+		snps<-setdiff(snps,drop)
+		df.trait3<-df.trait3[,- which(colnames(df.trait3) %in% drop)]
+		df3<-df3[,-which(colnames(df3) %in% drop)]
+		n.clean <- length(snps)
+	}
+	#run the model
+	f3 <- as.formula(paste("Y ~", paste(snps,collapse="+")))
+	modelsep<-diag(n.clean)
+	mod.trait3<-glib(x=df.trait3[,-1], y=df.trait3[,1]/3, error="binomial", link="logit",models=modelsep)
+	pp.trait3<-mod.trait3$bf$postprob[,2]
+	whichsnps3<-snps[which(pp.trait3>pp.thr)]
+	whichsnps<-union(union(whichsnps1,whichsnps2),whichsnps3)
+	snps<-intersect(whichsnps,snps)
+	n.clean <- length(snps)
+	cat("We consider ",n.clean, " snps in the final analysis. \n")
     #covert to a binomial model so we can run glib
     f1 <- as.formula(paste("Y ~ 1 | ", paste(snps,collapse="+")))
     binmod<-mlogit2logit(f1,data=df1,choices=0:3,base.choice = 1)$data
@@ -67,12 +143,36 @@ coloc.bayes.3t <- function(df1,snps=setdiff(colnames(df1),response),response="Y"
     binX<-binX[,-1]
     #extract the new reponse
     binY<-binmod[,"Y.star"]
+	## remove SNPs to ensure that binX is full rank when given an intercept
+	intbinX<-cbind(Intercept=1,binX)
+	intrank<-qr(intbinX)$rank
+	if ( intrank < ncol(intbinX)){
+		#we must remove at k columns
+		k<-ncol(intbinX)-intrank
+		drop<-c()
+		for(ii in 1:n.clean) {
+			if (intrank < ncol(intbinX)){ 
+				xsub <- intbinX[,setdiff(colnames(intbinX),paste0(c("z_1.","z_2."),snps[ii]))]
+				droprank<- qr(xsub)$rank
+				if (droprank>intrank-2){
+					drop<-c(drop,snps[ii])
+					intbinX<-xsub
+					intrank<-qr(intbinX)$rank
+				}
+			}
+		}
+		bindrop<-c(paste0("z_1.",drop),paste0("z_2.",drop))
+		snps<-setdiff(snps,drop)
+		binX<-binX[,- which(colnames(binX) %in% bindrop)]
+		n.clean <- length(snps)
+	}
     models<-makebinmod.3t(n.clean)
     category<-apply(models,1,whichcat.3t)
     #run glib
     mods1 <- glib(x=binX, y=binY, error="binomial", link="logit",models=models)
     #-2 log the bf with a flat prior
     twologB10=mods1$bf$twologB10[,2]
+    logB10<-0.5*twologB10
     logbf<-rep(0,15)
     for (ii in 1:15){
         logbf[ii]<-my.logsum(0.5*twologB10[which(category==(ii-1))])  
@@ -85,13 +185,12 @@ coloc.bayes.3t <- function(df1,snps=setdiff(colnames(df1),response),response="Y"
         postprob<-postprob/sum(postprob)
         cat("for prior: ", prior/sum(prior), "\n")
         cat("we have posterior: ",postprob, "\n")
-        cat("PP for shared variant is: ", 100*postprob[15], "% \n")
         cat("--- \n")
         postprobs[[ii]]=postprob
     }
     tmp <- 0.5*twologB10[which(category==14)]
     pp<-(exp( tmp - logsum(tmp) ))
-    return(list("postprob"=pp,"snps"=snps))
+    return(list("postprob"=pp,"snps"=snps,"models"=models,"bf"=logB10))
 }
 
 
@@ -108,7 +207,7 @@ coloc.bayes.3t <- function(df1,snps=setdiff(colnames(df1),response),response="Y"
 #'@param response The name of the response variable in \code{df1}
 #'@param priors A list of priors over the hypotheses 
 #'@param pp.thr posterior probability threshold used to trim SNP list.  Only SNPs with a marginal posterior probability of inclusion greater than this with one or other trait will be included in the full BMA analysis
-#'@param r2.trim If a pairs of SNPs has r2>\code{r2.trim}, they are put in the same tag
+#'@param r2.trim If a pairs of SNPs has r2 greater than \code{r2.trim}, they are put in the same tag
 #'@param quiet suppress messages about how the model spaced is trimmed for BMA
 #'@return a list of posterior probabilities that each tag is causitive to both traits, the tag names, and the corresponding SNPs
 #'@author Mary Fortune
@@ -141,22 +240,103 @@ coloc.bayes.3t.tag <- function(df1,snps=setdiff(colnames(df1),response),response
     n.clean <- length(tags)
  
     #remove SNPs with low posterior probabilities in the individual models.
-    f1 <- as.formula(paste("Y ~", paste(tags,collapse="+")))
-    df.trait1<-df1[which(df1[,1] %in% c(0,1)),c("Y",tags)]
-    df.trait2<-df1[which(df1[,1] %in% c(0,2)),c("Y",tags)]
-    df.trait3<-df1[which(df1[,1] %in% c(0,3)),c("Y",tags)]
-    df.trait2[,1]<-df.trait2[,1]/2
-    df.trait3[,1]<-df.trait3[,1]/3
-    modelsep<-diag(n.clean)
-    mod.trait1<-glib(x=df.trait1[,-1], y=df.trait1[,1], error="binomial", link="logit",models=modelsep)
-    mod.trait2<-glib(x=df.trait2[,-1], y=df.trait2[,1], error="binomial", link="logit",models=modelsep)
-    mod.trait3<-glib(x=df.trait3[,-1], y=df.trait3[,1], error="binomial", link="logit",models=modelsep)
-    pp.trait1<-mod.trait1$bf$postprob[,2]
-    pp.trait2<-mod.trait2$bf$postprob[,2]
-    pp.trait3<-mod.trait3$bf$postprob[,2]
-    whichtags<-sort(union(union(which(pp.trait1>pp.thr),which(pp.trait2>pp.thr)),which(pp.trait3>pp.thr)))
-    cat("We consider ",length(whichtags), " tags in the final analysis. \n")
-    tags<-tags[whichtags]
+    ##########
+	#remove tags with low posterior probabilities in the individual models.
+	#extract just those samples relating to trait1
+	df.trait1<-df1[which(df1[,1]%in% c(0,1)),c("Y",tags)]
+	#remove columns if necessary to ensure a full rank X
+	intX1<-cbind(Intercept=1,df.trait1[,-1])
+	intrank<-qr(intX1)$rank
+	if ( intrank < ncol(intX1)){
+		#we must remove at k columns
+		k<-ncol(intX1)-intrank
+		drop<-c()
+		for(ii in 1:n.clean) { 
+			if (length(drop)<k){ 
+				xsub <- intX1[,setdiff(colnames(intX1),tags[ii])]
+				droprank<- qr(xsub)$rank
+				if (droprank==intrank){
+					drop<-c(drop,tags[ii])
+					intX1<-xsub
+				}
+			}
+		}
+		tags<-setdiff(tags,drop)
+		df.trait1<-df.trait1[,- which(colnames(df.trait1) %in% drop)]
+		df1<-df1[,-which(colnames(df1) %in% drop)]
+		n.clean <- length(tags)
+	}
+	#run the model
+	f1 <- as.formula(paste("Y ~", paste(tags,collapse="+")))
+	modelsep<-diag(n.clean)
+	mod.trait1<-glib(x=df.trait1[,-1], y=df.trait1[,1], error="binomial", link="logit",models=modelsep)
+	pp.trait1<-mod.trait1$bf$postprob[,2]
+	whichtags1<-tags[which(pp.trait1>pp.thr)]
+	#extract just those samples relating to trait2
+	df.trait2<-df1[which(df1[,1]%in% c(0,2)),c("Y",tags)]
+	#remove columns if necessary to ensure a full rank X
+	intX2<-cbind(Intercept=1,df.trait2[,-1])
+	intrank<-qr(intX2)$rank
+	if ( intrank < ncol(intX2)){
+		#we must remove at k columns
+		k<-ncol(intX2)-intrank
+		drop<-c()
+		for(ii in 1:n.clean) { 
+			if (length(drop)<k){ 
+				xsub <- intX2[,setdiff(colnames(intX2),tags[ii])]
+				droprank<- qr(xsub)$rank
+				if (droprank==intrank){
+					drop<-c(drop,tags[ii])
+					intX2<-xsub
+				}
+			}
+		}
+		tags<-setdiff(tags,drop)
+		df.trait2<-df.trait2[,- which(colnames(df.trait2) %in% drop)]
+		df1<-df1[,-which(colnames(df1) %in% drop)]
+		n.clean <- length(tags)
+	}
+	#run the model
+	f2 <- as.formula(paste("Y ~", paste(tags,collapse="+")))
+	modelsep<-diag(n.clean)
+	mod.trait2<-glib(x=df.trait2[,-1], y=df.trait2[,1]/2, error="binomial", link="logit",models=modelsep)
+	pp.trait2<-mod.trait2$bf$postprob[,2]
+	whichtags2<-tags[which(pp.trait2>pp.thr)]
+	#extract just those samples relating to trait3
+	df.trait3<-df1[which(df1[,1] %in% c(0,3)),c("Y",tags)]
+	#remove columns if necessary to ensure a full rank X
+	intX3<-cbind(Intercept=1,df.trait3[,-1])
+	intrank<-qr(intX3)$rank
+	if ( intrank < ncol(intX3)){
+		#we must remove at k columns
+		k<-ncol(intX3)-intrank
+		drop<-c()
+		for(ii in 1:n.clean) { 
+			if (length(drop)<k){ 
+				xsub <- intX3[,setdiff(colnames(intX3),tags[ii])]
+				droprank<- qr(xsub)$rank
+				if (droprank==intrank){
+					drop<-c(drop,tags[ii])
+					intX3<-xsub
+				}
+			}
+		}
+		tags<-setdiff(tags,drop)
+		df.trait3<-df.trait3[,- which(colnames(df.trait3) %in% drop)]
+		df1<-df1[,-which(colnames(df1) %in% drop)]
+		n.clean <- length(tags)
+	}
+	#run the model
+	f3 <- as.formula(paste("Y ~", paste(tags,collapse="+")))
+	modelsep<-diag(n.clean)
+	mod.trait3<-glib(x=df.trait3[,-1], y=df.trait3[,1]/3, error="binomial", link="logit",models=modelsep)
+	pp.trait3<-mod.trait3$bf$postprob[,2]
+	whichtags3<-tags[which(pp.trait3>pp.thr)]
+	whichtags<-union(union(whichtags1,whichtags2),whichtags3)
+	tags<-intersect(whichtags,tags)
+	n.clean <- length(tags)
+	cat("We consider ",n.clean, " tags in the final analysis. \n")
+
     
     n.clean <- length(tags)
     #covert to a binomial model so we can run glib
@@ -168,6 +348,29 @@ coloc.bayes.3t.tag <- function(df1,snps=setdiff(colnames(df1),response),response
     binX<-binX[,-1]
     #extract the new reponse
     binY<-binmod[,"Y.star"]
+	## remove SNPs to ensure that binX is full rank when given an intercept
+	intbinX<-cbind(Intercept=1,binX)
+	intrank<-qr(intbinX)$rank
+	if ( intrank < ncol(intbinX)){
+		#we must remove at k columns
+		k<-ncol(intbinX)-intrank
+		drop<-c()
+		for(ii in 1:n.clean) {
+			if (intrank < ncol(intbinX)){ 
+				xsub <- intbinX[,setdiff(colnames(intbinX),paste0(c("z_1.","z_2."),tags[ii]))]
+				droprank<- qr(xsub)$rank
+				if (droprank>intrank-2){
+					drop<-c(drop,tags[ii])
+					intbinX<-xsub
+					intrank<-qr(intbinX)$rank
+				}
+			}
+		}
+		bindrop<-c(paste0("z_1.",drop),paste0("z_2.",drop))
+		tags<-setdiff(tags,drop)
+		binX<-binX[,- which(colnames(binX) %in% bindrop)]
+		n.clean <- length(tags)
+	}
     models<-makebinmod.3t(n.clean)
     category<-apply(models,1,whichcat.3t)
     #run glib
@@ -240,7 +443,6 @@ coloc.bayes.3t.tag <- function(df1,snps=setdiff(colnames(df1),response),response
         postprob<-postprob/sum(postprob)
         cat("for prior: ", prior/sum(prior), "\n")
         cat("we have posterior: ",postprob, "\n")
-        cat("PP for shared variant is: ", 100*postprob[15], "% \n")
         cat("--- \n")
         postprobs[[ii]]=postprob
     }
@@ -250,7 +452,7 @@ coloc.bayes.3t.tag <- function(df1,snps=setdiff(colnames(df1),response),response
     for (ii in 1:length(pp)){
         snplist[[ii]]<-names(which(tagkey==tags[ii]))
     }
-    return(list("postprob"=pp,"tags"=tags,snps=snplist))
+    return(list("postprob"=pp,"tags"=tags,"snps"=snplist,"models"=models,"bf"=logB10))
 }
 
 
@@ -373,74 +575,3 @@ my.logsum <- function(x) {
 }
 
 
-##' Internal function, wlogsum
-##'
-##' This function calculates the log of the sum of the exponentiated
-##' logs taking out the max, i.e. insuring that the sum is not Inf
-##' This sum is weighted by some constants w
-##' @title wlogsum
-##' @param x numeric vector
-##' @param w numeric vector
-##' @return my.max + log(sum(exp(x - my.max )*w))
-##' @author Chris Wallace
-wlogsum <- function(x, w=NULL) {
-    if (length(x)==1){
-        if(is.null(w)) {
-            return(x)
-        }else{
-            return(x*w)
-        }
-    }
-    my.max <- max(x) ##take out the maximum value in log form
-    if (my.max == -Inf){return(-Inf)}
-    if(is.null(w)) {
-        my.max + log(sum(exp(x - my.max )))
-    } else {
-        my.max + log(sum(exp(x - my.max )*w))
-    }
-}
-
-
-##'Returns the r2 values between each pair of SNPs
-##'@title find r2
-##'@param X a SnpMatrix
-##'@return a matrix of the r2 values
-##'@author Chris Wallace
-myr2 <- function(X) {
-  r2 <- ld(X,
-           depth=ncol(X)-1,
-           symmetric=TRUE,
-           stat="R.squared")
-if(any(is.na(r2))) {
-    r2.na <- as(is.na(r2),"matrix")
-    use <- rowSums(r2.na)>0
-## work around for r2=NA bug.
-    r2.cor <- as(cor(as(X[,use,drop=FALSE],"numeric"), use="pairwise.complete.obs")^2,"Matrix")
-    r2[ which(r2.na) ] <- r2.cor[ which(r2.na[use,use]) ]
-}
-  diag(r2) <- 1
-return(r2)
-}
-##' Derive tag SNPs for a SnpMatrix object using heirarchical clustering
-##'
-##' Uses complete linkage and the \code{\link{hclust}} function to define clusters, then cuts the tree at 1-tag.threshold
-##' @title tag
-##' @param snps colnames of the SnpMatrix object to be used
-##' @param tag.threshold threshold to cut tree, default=0.99
-##' @param samples optional, subset of samples to use
-##' @return character vector, names are \code{snps}, values are the tag for each SNP
-##' @author Chris Wallace
-##' @export
-tag <- function(X,tag.threshold=0.99, snps=NULL, samples=NULL) {
-if(!is.null(snps) || !is.null(samples))
-    X <- X[samples,snps]
-  r2 <- myr2(X)
-   D <- as.dist(1-r2)
-   hc <- hclust(D, method="complete")
-   clusters <- cutree(hc, h=1-tag.threshold)
-   snps.use <- names(clusters)[!duplicated(clusters)]
-   r2.use <- r2[snps.use, colnames(X), drop=FALSE]
-   tags <- rownames(r2.use)[apply(r2.use,2,which.max)]
-   names(tags) <- colnames(r2.use)
-return(tags)
-}
